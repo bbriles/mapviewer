6f3b6cbeee8ee8b36c4f832539745d0dd8806d04
Brad Briles <brilesb@gmail.com>
2019-01-09 22:25:47 -0800

    updated token list

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -21,7 +21,7 @@ export class MainScene extends Phaser.Scene {
 
   preload(): void {
     this.mapManager = new MapManager();
-    this.map = this.mapManager.loadMap("M00");
+    this.map = this.mapManager.loadMap("M01");
     this.load.image(this.map.name, "./assets/maps/"+this.map.file);
     this.load.spritesheet("tokens", "./assets/tokens/tokens.png", { frameWidth: 120, frameHeight: 120 });
     this.load.image("hide", "./assets/hide.png");
